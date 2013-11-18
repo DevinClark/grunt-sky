bc5ac617fd9d06874793f3f2dc51cac618820125
DevinClark <dclarkdesign@gmail.com>
2013-11-17 21:58:11 -0600

    Add line comment parsing. General Cleanup.

--- tasks/sky.js
+++ tasks/sky.js
@@ -11,45 +11,74 @@ module.exports = function(grunt) {
 
 	var _ = require('underscore');
 
+	var regexes = {
+		lineType: /\s?(\w{3,})\:\s/,
+		lineTypeOnly: /\s?(\w{3,})\:\s(.+)/,
+		jsDocStars: /^[\s|\t]{0,}\*\s{0,}/g,
+		url: /(^|\s)((?:https?|ftp):\/\/[\-A-Z0-9+\u0026@#\/%?=()~_|!:,.;]*[\-A-Z0-9+\u0026@#\/%=~()_|])/gi,
+		jsDocTag: /@(\w+)\s(.+)/gi,
+		jsDocTitleTag: /@title\s(.+)/gi
+	};
+
 	function autolink(text) {
 		return text.replace(regexes.url, "$1[$2]($2)");
 	}
 
 	function parseBlockComment(text) {
-		text = text.replace('*\n * ', '\n\n');
+		text = text.replace('*\n * ', '');
 		text = text.split('\n');
 		for (var i = 0; i < text.length; i++) {
-			text[i] = text[i].replace(/^\s\*\s?/g, '');
-			text[i] = text[i].replace(/\s+$/, '');
+			text[i] = text[i].replace(regexes.jsDocStars, '');
+			text[i] = text[i].trim();
+
+			if (text[i].match(regexes.jsDocTitleTag)) {
+				text.splice(i, 1);
+			}
 		}
+
+		/*text = _.reject(text, function reject(item) {
+			return item === "";
+		});*/
 
 		return text.join("\n");
 	}
 
-	function parseToDoComment(text) {
-		return "* " + text.replace(' TODO: ', '') + "\n";
+	function parseLineComment(text) {
+		return text.replace(regexes.lineType, '');
 	}
 
-	function parseFixMeComment(text) {
-		return "* " + text.replace(' FIXME: ', '') + "\n";
+	function extractJSDocTag(text) {
+		if (text.match(regexes.jsDocTag)) {
+			var matches = regexes.jsDocTag.exec(text);
+			if (matches !== null) {
+				//grunt.log.writeln(matches);
+
+				return {
+					type: matches[1],
+					description: matches[2]
+				}
+			}
+		}
 	}
 
 	grunt.registerMultiTask('sky', 'A simple Markdown JavaScript documentation plugin.', function registerJSDocTask() {
 		var esprima = require('esprima');
 			// Merge task-specific and/or target-specific options with these defaults.
 		var options = this.options({
-			punctuation: '.',
-			separator: ', '
+			header: "",
+			// TODO: Add option for a header file path.
+			lineComments: {
+				exclude: [],
+				include: []
+			}
 		});
 		var commentsObj = [];
-		var output = "";
-		var todos = "## TODO\n\n";
-		var bugs = "## Bugs\n\n";
+		var output = options.header || "";
+		var lineComments = [];
 
 		// Iterate over all specified file groups.
 		this.files.forEach(function(f) {
 			// Concat specified files.
-			grunt.log.writeln(f.src)
 			var fileGroup = grunt.file.expand({}, f.src);
 
 			fileGroup.forEach(function(file) {
@@ -64,29 +93,68 @@ module.exports = function(grunt) {
 
 			commentsObj.forEach(function(obj) {
 				// Loop through files
-				if (commentsObj.length > 1) {
-					// output headings for files.
-					output += "## " + obj.file + "\n";
+				var hasUserTitle;
+				obj.comments.forEach(function(comment) {
+					if (comment.type === "Block" && comment.value.match(/\*\n/)) {
+						var match = extractJSDocTag(comment.value);
+						if (match && match.type.toLowerCase() === "title") {
+							hasUserTitle = match.description;
+						}
+					}
+				});
+				var shouldDisplayTitle = _.some(obj.comments, function(comment) {
+					return comment.type === "Block" && comment.value.match(/\*\n/);
+				});
+
+				if (shouldDisplayTitle) {
+					if (hasUserTitle) {
+						output += "\n\n## " + hasUserTitle + "  \n";
+						output += "_" + obj.file + "_\n\n";
+					} else {
+						output += "\n\n## " + obj.file + "\n\n";
+					}
 				}
 
 				obj.comments.forEach(function(comment) {
 					if (comment.type === "Line") {
 						// Line comments
-						if (comment.value.indexOf(' TODO: ') === 0) {
-							todos += parseToDoComment(comment.value);
+						if (comment.value.match(regexes.lineType)) {
+							var type = comment.value.replace(regexes.lineTypeOnly, '$1');
+							lineComments.push({
+								type: type,
+								text: parseLineComment(comment.value)
+							})
 						}
-						else if (comment.value.indexOf(' FIXME: ') === 0) {
-							bugs += parseFixMeComment(comment.value);
-						}
-					} else if (comment.type === "Block" && comment.value.indexOf('*\n') == 0) {
+					} else if (comment.type === "Block" && comment.value.match(/\*\n/)) {
 						output += parseBlockComment(comment.value);
 					}
 				});
 
-				grunt.log.writeln(JSON.stringify(obj, null, 2));
 			});
 
-			output = output + "\n\n" + todos + "\n\n" + bugs;
+			output = output + "\n\n";
+
+			// Group the lines by type.
+			lineComments = _.groupBy(lineComments, 'type');
+
+			if (options && options.lineComments && options.lineComments.exclude) {
+				options.lineComments.exclude.forEach(function iteration(value) {
+					if (lineComments[value]) {
+						delete lineComments[value];
+					}
+				});
+			}
+
+			// Loop through and output each line comment type.
+			_.forEach(lineComments, function iteration(value, key) {
+				output += "\n## " + key + "\n\n";
+				value.forEach(function iteration(value) {
+					output += "* " + value.text + "\n";
+				});
+			});
+
+			//grunt.log.writeln(JSON.stringify(lineComments, null, 2));
+
 			output = autolink(output);
 			grunt.file.write(f.dest, output);
 
